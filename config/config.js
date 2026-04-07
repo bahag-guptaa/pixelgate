77d057b1a862bbeb1566df86983d3b681efb981d
Ayan Gupta <ayan.gupta@bahag.com>
2026-04-07 13:34:43 +0200

    refactor: restructure database configuration for dev and prod

--- config/config.js
+++ config/config.js
@@ -1,18 +1,21 @@
 import 'dotenv/config';
 
-export default {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const dbConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
   },
+};
+
+export default {
+  development: dbConfig,
+  production: dbConfig,
 };
